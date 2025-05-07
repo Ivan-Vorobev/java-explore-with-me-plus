aa48e96c40ef9d283083b1ab2a10513d0a64afca
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-05-07 23:21:49 +0300

    feat: добавлен mapstruct вместо mapper

--- stats/stats-server/src/main/java/ru/practicum/explorewithme/stats/mapper/QueryMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/explorewithme/stats/mapper/QueryMapper.java
@@ -1,16 +1,10 @@
 package ru.practicum.explorewithme.stats.mapper;
 
+import org.mapstruct.Mapper;
 import ru.practicum.dto.InputQueryRequest;
 import ru.practicum.explorewithme.stats.model.Query;
 
-public class QueryMapper {
-
-    public static Query mapToModel(InputQueryRequest inputQueryRequest) {
-        return Query.builder()
-                .app(inputQueryRequest.getApp())
-                .ip(inputQueryRequest.getIp())
-                .uri(inputQueryRequest.getUri())
-                .timestamp(inputQueryRequest.getTimestamp())
-                .build();
-    }
+@Mapper
+public interface QueryMapper {
+    Query mapToDTO(InputQueryRequest request);
 }
