9b71a0a9fdc19e5911b4332a359883ec367941c7
Vladislav Kotletkin <nkhzrd@yandex.ru>
2025-05-19 11:44:16 +0400

    feat: added fixes on new develop

--- main-service/src/main/java/ru/practicum/explorewithme/users/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/explorewithme/users/repository/RequestRepository.java
@@ -1,11 +1,15 @@
 package ru.practicum.explorewithme.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.explorewithme.users.model.ParticipationRequest;
+import ru.practicum.explorewithme.users.model.RequestStatus;
 
 import java.util.List;
 
 public interface RequestRepository extends JpaRepository<ParticipationRequest, Long> {
+
     List<ParticipationRequest> findParticipationRequestByRequester_Id(long userId);
 
     long countParticipationRequestByRequesterIdAndEvent_Id(long userId, long eventId);
@@ -13,6 +17,7 @@ public interface RequestRepository extends JpaRepository<ParticipationRequest, L
     long countParticipationRequestByEvent_Id(long eventId);
 
     List<ParticipationRequest> findParticipationRequestByRequester_IdAndEvent_Id(Long requesterId, Long eventId);
+
     @Query("SELECT r.event, count(r.id) as requestsCount FROM ParticipationRequest r WHERE r.status = :status AND r.event.id in :eventIds GROUP BY r.event ORDER BY requestsCount ASC")
     List<ParticipationRequest> findEventsCountByStatus(@Param("eventIds") List<Long> eventIds, @Param("status") RequestStatus status);
 }
