4ce20c1d9d1aed04fb75acd3b870cbee03e3f12c
Vladislav Kotletkin <nkhzrd@yandex.ru>
2025-05-21 12:05:18 +0400

    fix: added missing servlet exception processing

--- stats/stats-server/src/main/java/ru/practicum/ewm/exception/GlobalExceptionHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,11 +19,21 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleException(final Exception e) {
         log.error("500 {}", e.getMessage(), e);
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, "Error ...", e.getMessage(), getStackTrace(e));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ApiError handleOnMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        log.error("{} {}", HttpStatus.BAD_REQUEST, e.getMessage());
+        return new ApiError(HttpStatus.BAD_REQUEST, "MissingServletRequestParameterException Validation Exception.",
+                e.getMessage(), getStackTrace(e));
+    }
+
+    private String getStackTrace(Throwable e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
-        String stackTrace = sw.toString();
-        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR,"Error ...", e.getMessage(), stackTrace);
+        return sw.toString();
     }
-
 }
