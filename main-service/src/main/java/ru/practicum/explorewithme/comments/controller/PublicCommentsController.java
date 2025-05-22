72a6cef916a04a8adb71356a9a743f028bc70d6a
Vladislav Kotletkin <drzhkn1@mail.ru>
2025-05-22 11:21:41 +0300

    feat: added structure package for comments

---
+++ main-service/src/main/java/ru/practicum/explorewithme/comments/controller/PublicCommentsController.java
@@ -0,0 +1,4 @@
+package ru.practicum.explorewithme.comments.controller;
+
+public class PublicCommentsController {
+}
--- main-service/src/main/java/ru/practicum/explorewithme/events/dto/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/explorewithme/events/dto/NewEventDto.java
@@ -2,8 +2,12 @@ package ru.practicum.explorewithme.events.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
 import ru.practicum.explorewithme.config.CustomLocalDateTimeDeserializer;
 import ru.practicum.explorewithme.config.CustomLocalDateTimeSerializer;
 import ru.practicum.explorewithme.constraint.EventStartDateTime;
